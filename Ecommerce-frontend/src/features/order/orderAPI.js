6a55b49405eb8af348248ac45a1001fbf61d90c2
Khushi-H <154000684+Khushi-H@users.noreply.github.com>
2026-01-19 16:36:13 +0530

    Update orderAPI.js

--- Ecommerce-frontend/src/features/order/orderAPI.js
+++ Ecommerce-frontend/src/features/order/orderAPI.js
@@ -1,6 +1,6 @@
 export function createOrder(order) {
   return new Promise(async (resolve) => {
-    const response = await fetch("/api/orders", {
+    const response = await fetch("https://mern-ecommerce-backend-project.onrender.com/api/orders", {
       method: "POST",
       body: JSON.stringify(order),
       headers: { "content-type": "application/json" },
@@ -12,7 +12,7 @@ export function createOrder(order) {
 }
 export function updateOrder(update) {
   return new Promise(async (resolve) => {
-    const response = await fetch("/api/orders/" + update.id, {
+    const response = await fetch("https://mern-ecommerce-backend-project.onrender.com/api/orders/" + update.id, {
       method: "PATCH",
       body: JSON.stringify(update),
       headers: { "content-type": "application/json" },
@@ -30,7 +30,7 @@ export function fetchAllOrders(pagination) {
   }
 
   return new Promise(async (resolve) => {
-    const response = await fetch("/api/orders?" + queryString);
+    const response = await fetch("https://mern-ecommerce-backend-project.onrender.com/api/orders?" + queryString);
     const data = await response.json();
     resolve({ data });
   });
